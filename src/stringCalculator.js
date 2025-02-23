2b96ac986d41c172980ef7232c758e2acb8a8be6
Vivek Somasundaram <viveksomasundaram@Viveks-MacBook-Air.local>
2025-02-23 20:42:14 +0530

    Throw error for negative numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -10,5 +10,12 @@ export default function add(numbers) {
         }
     }
 
-    return numbers.split(delimiter).map(Number).reduce((sum, num) => sum + num, 0);
+    const numArray = numbers.split(delimiter).map(Number);
+    const negatives = numArray.filter(num => num < 0);
+
+    if (negatives.length) {
+        throw new Error(`Negative numbers not allowed: ${negatives.join(", ")}`);
+    }
+
+    return numArray.reduce((sum, num) => sum + num, 0);
 }
--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -23,3 +23,7 @@ test("handles new lines between numbers", () => {
 test("supports different delimiters", () => {
     expect(add("//;\n1;2;3")).toBe(6);
 });
+
+test("throws error for negative numbers", () => {
+    expect(() => add("1,-2,3,-4")).toThrow("Negative numbers not allowed: -2, -4");
+});
